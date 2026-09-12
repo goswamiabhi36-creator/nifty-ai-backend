67a2335ef71beb9a8a1076a82c9d3650473aaeee
goswamiabhi36-creator <goswamiabhi36@gmail.com>
2026-09-12 19:00:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,17 +31,4 @@ def nifty():
         "signal": "NEUTRAL",
         "trend": "UNKNOWN",
         "time": datetime.now().isoformat()
-    }            "price": info.get("last"),
-            "change": info.get("change"),
-            "change_percent": info.get("percentChange"),
-            "market_status": "live",
-            "time": datetime.now().isoformat()
-        }
-
-    except Exception as e:
-        return {
-            "symbol": "NIFTY 50",
-            "price": None,
-            "status": "data_error",
-            "error": str(e)
-        }
+    }
